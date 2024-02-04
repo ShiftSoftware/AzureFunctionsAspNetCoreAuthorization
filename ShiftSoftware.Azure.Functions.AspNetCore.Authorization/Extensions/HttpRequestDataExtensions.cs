18955f8e7da5eecfe75450798d5cb2534e99a022
Nahro <nahropro@gmail.com>
2024-02-05 00:05:59 +0300

    GetUser extension method on HttpRequestData to get user claims

---
+++ ShiftSoftware.Azure.Functions.AspNetCore.Authorization/Extensions/HttpRequestDataExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.Functions.Worker.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftSoftware.Azure.Functions.AspNetCore.Authorization.Extensions;
+
+public static class HttpRequestDataExtensions
+{
+    public static ClaimsPrincipal GetUser(this HttpRequestData req)
+    {
+        if(req.FunctionContext.Items["User"] is not null)
+            return req.FunctionContext.Items["User"] as ClaimsPrincipal;
+        else
+            return new ClaimsPrincipal();
+    }
+}
