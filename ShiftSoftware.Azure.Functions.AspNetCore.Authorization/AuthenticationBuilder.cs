2d4c822547b5dca8ca19286f1f630c592c195bef
Nahro Fuad <nahropro@gmail.com>
2024-06-11 22:19:06 +0300

    Register middlewares only for http trigger functions

--- ShiftSoftware.Azure.Functions.AspNetCore.Authorization/AuthenticationBuilder.cs
+++ ShiftSoftware.Azure.Functions.AspNetCore.Authorization/AuthenticationBuilder.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using ShiftSoftware.Azure.Functions.AspNetCore.Authorization.Services;
+using System.Reflection.PortableExecutable;
 
 namespace ShiftSoftware.Azure.Functions.AspNetCore.Authorization;
 
@@ -19,7 +20,10 @@ public class AuthenticationBuilder
     public AuthenticationBuilder AddJwtBearer(string authenticationScheme,
         TokenValidationParameters tokenValidationParameters)
     {
-        builder.UseMiddleware<AuthorizationMiddleware>();
+        builder.UseWhen<AuthorizationMiddleware>(context =>
+            context.FunctionDefinition.InputBindings.Any(binding => binding.Value.Type == "httpTrigger")
+        );
+
         builder.Services.AddScoped(x => new AuthorizationOptions(tokenValidationParameters, authenticationScheme));
         builder.Services.AddScoped<TokenService>();
 
