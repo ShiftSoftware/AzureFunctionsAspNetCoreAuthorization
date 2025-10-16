b016d094710a51cc029901fdbbf3d4f64de270b9
Nahro Fuad <nahropro@gmail.com>
2025-10-16 11:41:56 +0300

    Refactor authentication extension method

---
+++ ShiftSoftware.Azure.Functions.AspNetCore.Authorization/Extensions/FunctionsApplicationBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Builder;
+
+namespace ShiftSoftware.Azure.Functions.AspNetCore.Authorization.Extensions;
+
+public static class FunctionsApplicationBuilderExtensions
+{
+    public static AuthenticationBuilder AddAuthentication(this FunctionsApplicationBuilder builder)
+    {
+        return new AuthenticationBuilder(builder);
+    }
+}
--- ShiftSoftware.Azure.Functions.AspNetCore.Authorization/Extensions/IFunctionsWorkerApplicationBuilderExtension.cs
+++ ShiftSoftware.Azure.Functions.AspNetCore.Authorization/Extensions/IFunctionsWorkerApplicationBuilderExtension.cs
@@ -4,7 +4,6 @@ namespace ShiftSoftware.Azure.Functions.AspNetCore.Authorization.Extensions;
 
 public static class IFunctionsWorkerApplicationBuilderExtension
 {
-
     public static AuthenticationBuilder AddAuthentication(this IFunctionsWorkerApplicationBuilder builder)
     {
         return new AuthenticationBuilder(builder);
